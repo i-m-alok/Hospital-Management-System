b39abffd79838c4dec54aa10cb01af709f403932
AlokMishra051298 <Mishra051298alok@email.com>
2020-06-29 23:13:19 +0530

    Fixed few things

--- application/__init__.py
+++ application/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Mishra981205@localhost/Hospital"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:*******/Hospital"
 db = SQLAlchemy(app)
 
 from application import routes
