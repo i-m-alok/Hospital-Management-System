4d1a0d1d951c4b415008900e012547be2e8f9898
Alok051298Mishra <Mishra051298alok@gmail.com>
2020-07-19 02:01:23 +0530

    App deployed

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object(Config)
 # if app.debug == True:
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Mishra981205@localhost/testing"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:************@localhost/testing"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # else: 
 #     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ngdhvhfmbhjnxz:7d2fe20442e5a276d103e9f8aab0cbf4c653e2204204f471e3ca59f36f9029e7@ec2-54-234-44-238.compute-1.amazonaws.com:5432/dfo5htqi24l5ek'
